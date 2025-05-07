2099e074016a7888e4b10ffd9e3e0cc6df142942
matheus2306 <math2306.r@gmail.com>
2025-05-07 16:55:53 -0300

    feat: add navigation buttons to HeroCarousel for improved user experience

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -43,6 +43,24 @@ const HeroCarousel = (props) => {
           />
         ))}
       </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon visually-hidden" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon visually-hidden" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
   );
 };
